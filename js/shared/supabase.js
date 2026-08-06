429d1870f350329828da6f66c27a142d4dd26844
jsurban <fdc.julz@gmail.com>
2026-08-06 10:56:16 +0800

    Modified register.js for inserting user in DB

--- js/shared/supabase.js
+++ js/shared/supabase.js
@@ -1,12 +1,26 @@
 // Supabase Configuration
-const SUPABASE_URL = "https://ykldzfsbmlcjftfartax.supabase.co";
+// const SUPABASE_URL = "https://ykldzfsbmlcjftfartax.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrbGR6ZnNibWxjamZ0ZmFydGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4MzEyMDgsImV4cCI6MjEwMTQwNzIwOH0.HRygR3SId4YqtEwTFvE8tb5g7rnZtGJXXPNu7FLYlMs";
+// const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrbGR6ZnNibWxjamZ0ZmFydGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4MzEyMDgsImV4cCI6MjEwMTQwNzIwOH0.HRygR3SId4YqtEwTFvE8tb5g7rnZtGJXXPNu7FLYlMs";
 
 // Create ONE global client
-window.db = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+// window.db = window.supabase.createClient(
+ //    SUPABASE_URL,
+//     SUPABASE_ANON_KEY
+// );
 
-console.log("Supabase client initialized:", window.db);
+// console.log("Supabase client initialized:", window.db);
+
+const SUPABASE_URL = "https://ykldzfsbmlcjftfartax.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrbGR6ZnNibWxjamZ0ZmFydGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4MzEyMDgsImV4cCI6MjEwMTQwNzIwOH0.HRygR3SId4YqtEwTFvE8tb5g7rnZtGJXXPNu7FLYlMs";
+
+if (!window.supabase) {
+    console.error("Supabase CDN not loaded.");
+} else {
+    window.db = window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
+
+    console.log("Supabase initialized:", window.db);
+}
