ecd7f3bd96c71e262f58b0ffa213fcf13891c55a
jsurban <fdc.julz@gmail.com>
2026-08-06 23:13:46 +0800

    Modified profile page

--- js/user/profile.js
+++ js/user/profile.js
@@ -15,15 +15,28 @@ window.addEventListener("click", (e) => {
 });
 
 // Load current user
-const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const user = JSON.parse(localStorage.getItem("currentUser"));
 
-if (currentUser) {
+if (!user){
+    window.location.href="login.html";
+}
 
-    document.getElementById("profile-name").textContent =
-        `${currentUser.firstname} ${currentUser.lastname}`;
+document.getElementById("firstname").value = user.firstname;
+document.getElementById("lastname").value = user.lastname;
+document.getElementById("email").value = user.email;
+document.getElementById("role").value = user.role;
 
-    document.getElementById("profile-email").textContent =
-        currentUser.email;
+document.getElementById("profileFullName").textContent =
+`${user.firstname} ${user.lastname}`;
+
+document.getElementById("profileRole").textContent =
+user.role.toUpperCase();
+
+if(user.created_at){
+
+    document.getElementById("created_at").value =
+        new Date(user.created_at).toLocaleDateString();
+
 }
 
 // Logout
